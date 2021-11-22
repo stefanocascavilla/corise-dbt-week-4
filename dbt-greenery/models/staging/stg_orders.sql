ced1b126a6eba962240e1621ef214487a4203f60
stefanocascavilla <cascavilla1@gmail.com>
2021-11-22 10:27:05 +0000

    Fixed model

--- dbt-greenery/models/staging/stg_orders.sql
+++ dbt-greenery/models/staging/stg_orders.sql
@@ -18,20 +18,3 @@ with orders as (
 
 select *
 from orders
-
-
-
-with users_orders as (
-    select
-        u.user_id,
-        count(distinct o.order_id) as number_of_orders
-    from dbt_stefano_c.stg_users as u
-        inner join dbt_stefano_c.stg_orders as o
-        on u.user_id = o.user_id
-    group by
-        u.user_id
-)
-
-select
-    sum(case when number_of_orders > 1 then 1 end)::decimal / count(*) as repeat_rate
-from users_orders;
